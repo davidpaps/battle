bec77eba15bc8427542d64740e72f29210c01be6
David Papamichael <davidpapamichael@gmail.com>
2020-04-01 11:38:07 +0100

    File structure cleaned up, README and .gitignore added

--- spec/features/enter_name_spec.rb
+++ spec/features/enter_name_spec.rb
@@ -1,11 +1,11 @@
-feature 'enter name' do 
+feature 'enter name' do
 
-  scenario 'prints a form to the page' do 
+  scenario 'prints a form to the page' do
     visit('/')
     expect(page).to have_content 'Player 1 Name:'
   end
 
-  scenario 'enters info into the form' do 
+  scenario 'enters info into the form' do
     sign_in_and_play
     expect(page).to have_content 'david vs. goliath'
   end
